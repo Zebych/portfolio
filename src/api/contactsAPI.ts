991af493ce3248071b11ec7f9e835ca4cc51dbb8
Zebych <81613417+Zebych@users.noreply.github.com>
2022-01-02 22:48:11 +0300

    build deploy heroku

--- src/api/contactsAPI.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const contactsAPI = {
-    message(name: string , email: string , message: string) {
-        return axios.post("https://smtp-portfolio.herokuapp.com/sendMessage", {name, email, message})
-    }
-}
--- src/common/enums/projectDescription.ts
+++
@@ -1,7 +0,0 @@
-export enum ProjectDescription {
-    Shop = ' React + redux-toolkit + redux-thunk + typescript + eslint/prettier',
-    TodoList = ' React + redux + redux-thunk + typescript',
-    TodoListToolkit = 'Migration to redux-toolkit',
-    TodoListSaga = 'Migration to redux-saga',
-    SocialNetwork = 'React(on class components) + redux + redux-thunk + typescript'
-}
